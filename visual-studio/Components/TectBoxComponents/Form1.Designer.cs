d39a8d268fb711b93bc6a2ea6e0ec46a18a084a0
Álamo Vinícius <alamo.cma22@gmail.com>
2022-03-30 16:04:34 -0300

    upload files

---
+++ visual-studio/Components/TectBoxComponents/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿
+namespace TectBoxComponents
+{
+    partial class Form_main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_add = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox_vehicle = new System.Windows.Forms.TextBox();
+            this.textBox_listVehicles = new System.Windows.Forms.TextBox();
+            this.btn_clear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn_add
+            // 
+            this.btn_add.Location = new System.Drawing.Point(210, 44);
+            this.btn_add.Name = "btn_add";
+            this.btn_add.Size = new System.Drawing.Size(75, 23);
+            this.btn_add.TabIndex = 0;
+            this.btn_add.Text = "Adicionar";
+            this.btn_add.UseVisualStyleBackColor = true;
+            this.btn_add.Click += new System.EventHandler(this.btn_add_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(121, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Digite um Veículo:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textBox_vehicle
+            // 
+            this.textBox_vehicle.Location = new System.Drawing.Point(12, 44);
+            this.textBox_vehicle.Name = "textBox_vehicle";
+            this.textBox_vehicle.Size = new System.Drawing.Size(192, 20);
+            this.textBox_vehicle.TabIndex = 2;
+            // 
+            // textBox_listVehicles
+            // 
+            this.textBox_listVehicles.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox_listVehicles.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.textBox_listVehicles.Location = new System.Drawing.Point(12, 70);
+            this.textBox_listVehicles.Multiline = true;
+            this.textBox_listVehicles.Name = "textBox_listVehicles";
+            this.textBox_listVehicles.Size = new System.Drawing.Size(273, 244);
+            this.textBox_listVehicles.TabIndex = 3;
+            // 
+            // btn_clear
+            // 
+            this.btn_clear.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_clear.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.btn_clear.Font = new System.Drawing.Font("Microsoft YaHei", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_clear.Location = new System.Drawing.Point(15, 326);
+            this.btn_clear.Name = "btn_clear";
+            this.btn_clear.Size = new System.Drawing.Size(268, 23);
+            this.btn_clear.TabIndex = 4;
+            this.btn_clear.Text = "Limpar";
+            this.btn_clear.UseVisualStyleBackColor = true;
+            this.btn_clear.Click += new System.EventHandler(this.btn_clear_Click);
+            // 
+            // Form_main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(295, 361);
+            this.Controls.Add(this.btn_clear);
+            this.Controls.Add(this.textBox_listVehicles);
+            this.Controls.Add(this.textBox_vehicle);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btn_add);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "Form_main";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Components";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_add;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox_vehicle;
+        private System.Windows.Forms.TextBox textBox_listVehicles;
+        private System.Windows.Forms.Button btn_clear;
+    }
+}
+
---
+++ visual-studio/Components/TectBoxComponents/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TectBoxComponents
+{
+    public partial class Form_main : Form
+    {
+        public Form_main()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_add_Click(object sender, EventArgs e)
+        {
+            if (textBox_vehicle.Text == "")
+            {
+                MessageBox.Show("Digite um veículo");
+                textBox_vehicle.Focus();
+                return;
+            }
+            else
+            {
+                textBox_listVehicles.Text += textBox_vehicle.Text + " - ";
+                textBox_vehicle.Clear();
+                textBox_vehicle.Focus();
+            }
+        }
+
+        private void btn_clear_Click(object sender, EventArgs e)
+        {
+            textBox_listVehicles.Clear();
+            textBox_vehicle.Clear();
+            textBox_vehicle.Focus();
+        }
+
+        
+    }
+}
